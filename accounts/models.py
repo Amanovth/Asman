c2f88876e91c26fa9437df5274594d279663d136
Amanovth <mirlanuchyngyz@gmail.com>
2024-03-16 21:45:57 +0600

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
@@ -7,6 +9,12 @@ from .managers import CustomUserManager
 
 
 class User(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        max_length=36
+    )
     username = models.CharField(
         _("username"),
         max_length=150,
